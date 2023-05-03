7c2fb5c2102c94b2d9da5a1b99ab8e705bad4494
brutalvik <vikcanada90@gmail.com>
2023-05-03 06:48:00 -0600

    Added drawer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,18 +6,23 @@ import { BsMoonFill, BsSunFill } from "react-icons/bs";
 import { toggleHamburger, toggleTheme } from "app/reducers/theme";
 import { handleToggle } from "features/functions";
 import { Squash as Hamburger } from "hamburger-react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerOverlay,
+  DrawerContent,
+} from "@chakra-ui/react";
 
 const Header: FC = () => {
   const dispatch = useAppDispatch();
   const { darkMode, hamburgerIsOpen } = useAppSelector((state) => state.theme);
+  const toggleDrawer = () => handleToggle(dispatch, toggleHamburger);
 
   return (
     <div className={styles.container}>
       <div className={styles.menu}>
-        <Hamburger
-          onToggle={() => handleToggle(dispatch, toggleHamburger)}
-          toggled={hamburgerIsOpen}
-        />
+        <Hamburger onToggle={toggleDrawer} toggled={hamburgerIsOpen} />
       </div>
       <div className={styles.theme}>
         {darkMode ? (
@@ -25,6 +30,31 @@ const Header: FC = () => {
         ) : (
           <BsSunFill onClick={() => handleToggle(dispatch, toggleTheme)} />
         )}
+      </div>
+      <div className={styles.drawer}>
+        <Drawer
+          isOpen={hamburgerIsOpen}
+          placement="left"
+          onClose={toggleDrawer}
+          size="xs"
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerBody>
+              <div className={styles.menuItem}>
+                <p>Home</p>
+                <p>About</p>
+                <p>Skills</p>
+                <p>Portfolio</p>
+                <p>Contact</p>
+              </div>
+            </DrawerBody>
+
+            <DrawerFooter textAlign="center">
+              Copyright 2023 @Vikram Kumar
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,7 +12,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>
 );
