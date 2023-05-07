b3f8f2bf1247343612da16f476d19ff61738edfd
Vikram <vikcanada90@gmail.com>
2023-05-07 00:40:08 -0600

    added minimalistic about me page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,11 +1,22 @@
 import { FC, Suspense } from "react";
 import SpinnerItem from "UI/Spinner/SpinnerItem";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import styles from "./About.module.css";
+import { aboutMeDescription } from "features/constants";
+import { Divider, Stack, Text } from "@chakra-ui/react";
+import Name from "UI/Name/Name";
 
 const About: FC = () => {
   return (
     <Suspense fallback={<SpinnerItem />}>
-      <SpinnerItem />
+      <div className={styles.container}>
+        <Stack direction="row" h="auto" p={2}>
+          <Name childName="About Me" />
+        </Stack>
+        <Stack direction="row" h="auto" p={2}>
+          <Divider orientation="vertical" />
+          <Text className={styles.text}>{aboutMeDescription}</Text>
+        </Stack>
+      </div>
     </Suspense>
   );
 };
--- src/features/constants.ts
+++ src/features/constants.ts
@@ -6,3 +6,5 @@ export const sequenceData = [
   "FULLSTACK DEVELOPER",
   1000,
 ];
+
+export const aboutMeDescription = `Full Stack Developer with over nine years of experience. Highly motivated, result-oriented, team player who uses his experience with the latest technology to contribute to the projects. Through my experience working on several projects, I have gained valuable working experience developing and deploying multiple Angular and React based websites with NodeJS and has developed back-end frameworks using NodeJS and Firebase. I have also used Typescript and RESTful APIs to develop web applications. Between my technical skills, communication skills, and collaboration skills, I could prove to be an asset to any team and a great support for the successful outcome of any project.`;
